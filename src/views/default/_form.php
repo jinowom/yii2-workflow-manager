e4cd46de6d959cd30b47810c4e2bbb6236dc605e
lzq <18418105@qq.com>
2022-10-30 17:07:48 +0800

    updata views

--- src/views/default/_form.php
+++ src/views/default/_form.php
@@ -7,27 +7,35 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+
 ?>
 
-<div class="workflow-default-form">
+<!-- Begin: Content -->
+<section id="content">
+    <div class="panel">
+        <div class="panel-body">
 
-    <?php $form = ActiveForm::begin([
-        'id' => 'Workflow',
-        'enableClientValidation' => false,
-        'errorSummaryCssClass' => 'error-summary alert alert-error'
-    ]) ?>
+            <?php $form = ActiveForm::begin([
+                'id' => 'Workflow',
+                'enableClientValidation' => false,
+                'errorSummaryCssClass' => 'error-summary alert alert-error'
+            ]) ?>
 
-    <?php echo $form->errorSummary($model); ?>
+            <?php echo $form->errorSummary($model); ?>
 
-    <?= $form->field($model, 'id')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'id')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+            <?= $form->field($model, 'name_cn')->textInput(['maxlength' => true]) ?>
 
-    <?= Html::submitButton('<span class="glyphicon glyphicon-check"></span> ' . ($model->isNewRecord ? Yii::t('workflow', 'Create') : Yii::t('workflow', 'Save')), [
-        'id' => 'save-' . $model->formName(),
-        'class' => 'btn btn-success'
-    ]) ?>
-    <?= Html::a(Yii::t('workflow', 'Cancel'), ['index'], ['class' => 'btn btn-default']) ?>
+            <?= Html::submitButton('<span class="glyphicon glyphicon-check"></span> ' . ($model->isNewRecord ? Yii::t('workflow', 'Create') : Yii::t('workflow', 'Save')), [
+                'id' => 'save-' . $model->formName(),
+                'class' => 'btn btn-success'
+            ]) ?>
+            <?= Html::a(Yii::t('workflow', 'Cancel'), ['index'], ['class' => 'btn btn-default']) ?>
 
-    <?php ActiveForm::end(); ?>
+            <?php ActiveForm::end(); ?>
 
-</div>
+        </div>
+    </div>
+</section>
 
