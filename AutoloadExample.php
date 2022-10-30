070fdf8f46fed88d5650d24f39b54e23353c2a81
lzq <18418105@qq.com>
2022-10-30 17:00:55 +0800

    add jinostart-workflow

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace jinowom\workflow\manager;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
